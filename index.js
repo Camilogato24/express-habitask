47341d83a68b1325fcbe1d149d2588bda77e0558
Camilo Fernandez <Camilogato24@users.noreply.github.com>
2024-01-31 11:34:21 -0500

    delete mode_modules

--- index.js
+++ index.js
@@ -1,49 +1,41 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize } = require('sequelize');
+const { Tarea, Usuario } = require('./models');
 
+// Relación entre Usuario y Tarea
+Usuario.hasMany(Tarea, { as: 'TareasCreadas', foreignKey: 'usuario_creador_id' });
+Usuario.hasMany(Tarea, { as: 'TareasAsignadas', foreignKey: 'usuario_asignado_id' });
 
-const sequelize = new Sequelize('db_habitask', 'admin', 'passAWSbd', {
+Tarea.belongsTo(Usuario, { as: 'Creador', foreignKey: 'usuario_creador_id' });
+Tarea.belongsTo(Usuario, { as: 'Asignado', foreignKey: 'usuario_asignado_id' });
+
+new Sequelize('db_habitask', 'admin', 'passAWSbd', {
   host: 'database-habitask.c1uw4wgcq9bw.us-east-1.rds.amazonaws.com',
   dialect: 'mysql',
 });
 
-const Usuario = sequelize.define('Usuario', {
-  nombre: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  contrasena: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-},{
-  // Desactivar las marcas de tiempo automáticas
-  timestamps: false,
-});
 exports.handler = async (event, context) => {
-  const { nombre, email, contrasena } = event;
-
-  // Permite el uso de async/await
   context.callbackWaitsForEmptyEventLoop = false;
 
   try {
-    // Inserta en la tabla Usuarios
-    await Usuario.create({ nombre, email, contrasena });
+    // Obtén los datos necesarios de event.body o de donde corresponda
+    const { titulo, usuario_creador_id, usuario_asignado_id } = JSON.parse(event.body);
+
+    // Crea la tarea
+    const tareaCreada = await Tarea.create({
+      titulo,
+      usuario_creador_id,
+      usuario_asignado_id,
+    });
+
     // Devuelve una respuesta exitosa
-    console.log()
     return {
       statusCode: 200,
-      body: JSON.stringify({ mensaje: 'Usuario registrado exitosamente' }),
+      body: JSON.stringify({ mensaje: 'Tarea creada exitosamente', tarea: tareaCreada }),
     };
   } catch (error) {
     // Maneja los errores y devuelve una respuesta de error
-    console.log(error);
+    console.error('Error al crear la tarea:', error);
     return {
-      error,
       statusCode: 500,
       body: JSON.stringify({ error: 'Error interno del servidor' }),
     };
